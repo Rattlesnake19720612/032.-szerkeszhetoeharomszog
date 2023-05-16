7cc5923ce33a87dcf56189de1b0633d2bb592a34
DGYKKSZKI <124397426+Rattlesnake19720612@users.noreply.github.com>
2023-05-16 22:12:35 +0200

    Eldönti, hogy szerkeszthető-e △

--- 032. szerkeszhetoeharomszog/Program.cs
+++ 032. szerkeszhetoeharomszog/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,3 +33,11 @@ namespace _032.szerkeszhetoeharomszog
         }
     }
 }
+
+/*Ez egy C# program, amely meghatározza, hogy létrehozható-e háromszög a megadott oldalakkal. A program bekéri a felhasználótól a háromszög két befogójának és az átfogójának hosszát, majd ellenőrzi, hogy bármely két oldal összege nagyobb-e a harmadik oldalnál. Ha ez a feltétel teljesül, a program kiírja, hogy létrehozható a háromszög, különben kiírja, hogy nem hozható létre.
+Íme, hogyan működik a program lépésről lépésre:
+A program a konzolra írja az üzeneteket, amelyek arra kérnek, hogy a felhasználó adja meg a háromszög két befogójának és az átfogójának hosszát.
+A program ezeket az értékeket egész számokként olvassa be a Convert.ToInt32 metódus és a Console.ReadLine használatával.
+A program ellenőrzi, hogy a két első oldal (befogo1 és befogo2) összege nagyobb-e a harmadik oldalnál (atfogo). Ha ez a feltétel igaz, a program kiírja az üzenetet, hogy létrehozható a háromszög. Ezt az ellenőrzést az if utasítással hajtja végre.
+Ha az if utasítás feltétele hamis, a program egy másik feltételt ellenőriz annak érdekében, hogy megnézze, létrehozható-e a háromszög. Ebben az esetben az ellenőrzés annak a feltételnek a vizsgálatát jelenti, hogy bármely két oldal összege nagyobb-e a harmadik oldalnál. Ha ez a feltétel igaz, a program kiírja az üzenetet, hogy létrehozható a háromszög. Ezt az ellenőrzést az else if utasítással hajtja végre.
+Ha mind az if, mind az else if utasítások feltételei hamisak, a program kiírja az üzenetet, hogy nem hozható létre a háromszög.*/
